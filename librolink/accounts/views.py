40928b7c13b8099d66168fb8c6eb3717eaffd2d0
Nicolás Butterfield <nicobutter@gmail.com>
2024-06-26 22:39:37 -0300

    cambios

--- librolink/accounts/views.py
+++ librolink/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')  
+        else:
+            messages.error(request, 'Nombre de usuario o contraseña incorrectos.')
+    return render(request, 'accounts/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')  
